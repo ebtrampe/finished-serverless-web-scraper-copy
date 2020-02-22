adaf0ca9e4645210ccc141f51eb7fc314ebedb42
Eileen Trampe <ebtrampe@gmail.com>
2020-02-22 15:18:19 +0800

    correction on the export syntax

--- helpers.js
+++ helpers.js
@@ -1,1 +1,1 @@
-export const regex = /(node\.js)|(graphql)|(react)|(mongodb)|(aws)|(dynamodb)|(lambda)|(codecommit)|(git)|(serverless)|(cloudformation)|(docker)|(kubernetes)|(internship)|(werkstudent)/gi;
+exports.regex = /(node\.js)|(graphql)|(react)|(mongodb)|(aws)|(dynamodb)|(lambda)|(codecommit)|(git)|(serverless)|(cloudformation)|(docker)|(kubernetes)|(internship)|(werkstudent)/gi;
